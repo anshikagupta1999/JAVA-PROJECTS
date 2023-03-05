d9739efdaab1f48204a42e842da14a2ed31cb9f5
Anshika Gupta <47243623+anshikagupta15@users.noreply.github.com>
2023-03-05 16:25:08 +0530

    CWH1

---
+++ src/com/company/CWH/Derived.java
@@ -0,0 +1,4 @@
+package com.company.CWH;
+
+public class Derived {
+}
---
+++ src/com/company/CWH/Main.java
@@ -0,0 +1,9 @@
+package com.company.CWH;
+public class Main
+{
+    public static void main(String[] args)
+    {   //String name=new String("Anshika");
+        String name="Anshika";
+        System.out.println(name);
+    }
+}
